fb349d2f07d7c137937d205d43dd6bf45884e5f6
”迪迪“ <dd1279078070@gmail.com>
2023-05-27 17:59:39 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@
     <h3>人员列表</h3>
     <div class="right">
       <h4>人员: </h4>
-      <input class="search" v-model="searchKeyword" placeholder="请输入搜索内容" />
+      <input class="search" v-model="searchKeyword" placeholder="请输入搜索内容" @keyup="handleKeyUp" />
       <button @click=search>搜索</button>
     </div>
-    <el-table :data="tableData" border stripe style="width: 100%" height="430">
+    <el-table :data="tableData.slice((currentPage - 1) * PageSize, currentPage * PageSize)" border stripe style="width: 100%" height="430">
     <el-table-column prop="id" label="序号" sortable :sort-method="customSort"></el-table-column>
     <el-table-column prop="name" label="人员" sortable :sort-method="customSort"></el-table-column>
     <el-table-column prop="advance" label="推进中" sortable :sort-method="customSort"></el-table-column>
@@ -20,13 +20,15 @@
     </el-table-column>
   </el-table>
     <el-pagination
-      background
-      layout="prev, pager, next"
-      :total="filteredData.length"
-      :page-size="20"
-      :current-page="currentPage"
-      @current-change="handleCurrentChange">
-    </el-pagination>
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page.sync="currentPage"
+        :page-size="PageSize"
+        layout="prev, pager, next"
+        :total="total"
+        background
+      >
+      </el-pagination>
   </div>
 </template>
 
@@ -42,7 +44,9 @@ export default {
       tableData: [],
       searchKeyword: '',
       originalData: [],  //备份数据
-      currentPage: 1,
+      currentPage: 1, //当前页
+      total: 100,
+      PageSize: 20,
     };
   },
   mounted() {
@@ -76,8 +80,20 @@ export default {
       // 自定义排序方法的实现
       return a.id - b.id;
     },
-    handleCurrentChange(page) {
-      this.currentPage = page;
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+      this.currentPage = 1;
+      this.pageSize = val;
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+      this.currentPage = val;
+    },
+    handleKeyUp(event) {
+      if (event.keyCode === 8 && this.searchKeyword === '') {
+        // this.search();
+        this.fetchData();
+      }
     },
 
   },
